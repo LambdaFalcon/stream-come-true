c0a9809c09ddb276b0e74f44102ff537444083ac
Brenda Ruiz <brendaruizm@hotmail.com>
2019-04-27 13:11:29 +0200

    Resolved code review comments

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const {env} = require('../config.js')
+const path = require('path');
+const router = express.Router();
+const app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Stream Come True' });
-});
+if(env === 'development') {
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Stream Come True' });
+  });  
 
+} else if(env === 'production') {
+  router.use(express.static(path.resolve(__dirname, '../../client/build')));
+  router.get('/', function(req, res) {
+    
+    res.sendFile('index.html');
+  });
+}
 module.exports = router;
