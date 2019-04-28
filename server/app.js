b89d368e9758cfb8f35e3e631a51fefe63188bb0
falcon <aron.fiechter@gmail.com>
2019-04-29 00:38:51 +0200

    Set CORS in server app.js before registering routers
    Also update package-lock.json.

--- server/app.js
+++ server/app.js
@@ -13,14 +13,19 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// allow all CORS
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// register routers
 app.use('/', indexRouter);
 app.use('/testAPI', testAPIRouter);
-app.use(cors());
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
