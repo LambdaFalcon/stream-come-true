c79e78a6bcbc6c868d324e9e114083c3894cbd22
falcon <aron.fiechter@gmail.com>
2019-04-29 00:44:07 +0200

    Add Main component stub
    Also change server and client directory name variables in Gitlab CI config to see if pipeline triggers.

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends React.Component{
-  
+import Main from "./components/Main";
+
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { apiResponse: "" };
+    this.state = { testApiResponse: "" };
   }
-  
+
   callAPI() {
-    fetch('http://localhost:9000/testAPI')
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res }));
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ testApiResponse: res }));
   }
+
   componentWillMount() {
     this.callAPI();
   }
-  render(){
+
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.apiResponse}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Main />
+        <footer>{this.state.testApiResponse}</footer>
       </div>
     );
   }
