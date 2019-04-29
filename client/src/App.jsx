424265e078528f1cf7fd19dcabcea814a591e941
falcon <aron.fiechter@gmail.com>
2019-04-29 12:08:10 +0200

    Remove call to testAPI and handling of response from client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,31 +2,11 @@ import React from "react";
 import "./App.css";
 
 import Main from "./components/Main";
-import config from "./config";
-
-const { api } = config;
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { testApiResponse: "" };
-  }
-
-  callAPI() {
-    fetch(`${api}/testAPI`)
-      .then(res => res.text())
-      .then(res => this.setState({ testApiResponse: res }));
-  }
-
-  componentWillMount() {
-    this.callAPI();
-  }
-
   render() {
     return (
       <div className="App">
         <Main />
-        <footer>{this.state.testApiResponse}</footer>
       </div>
     );
   }
