b9d0ca13f0bab2b3e740b7c8201fe5c7eea34b84
Bojken Sina <sinab@usi.ch>
2019-05-05 17:47:02 +0200

    Create the sample chart for most keywords

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -1,5 +1,6 @@
 import React from "react"
-class MostWords extends React.Component{
+import {BarChart,CartesianGrid,XAxis,YAxis,Tooltip,Legend} from "recharts"
+class Bar extends React.Component{
     render(){
       return(
         <div className="col-xs-6">
@@ -8,11 +9,78 @@ class MostWords extends React.Component{
             {this.props.name}
           </div>
           <div className="panel-body">
+            <Graph data={this.props.data}/>
           </div>
         </div>
       </div>
       );
     }
   }
-export default MostWords;
+
+class Graph extends React.Component{
+  constructor(props){
+    super(props);
+    this.api = this.props.data;
+    this.state = {
+      data: [
+        {
+          "name": "Page A",
+          "uv": 4000,
+          "pv": 2400,
+          "amt": 2400
+        },
+        {
+          "name": "Page B",
+          "uv": 3000,
+          "pv": 1398,
+          "amt": 2210
+        },
+        {
+          "name": "Page C",
+          "uv": 2000,
+          "pv": 9800,
+          "amt": 2290
+        },
+        {
+          "name": "Page D",
+          "uv": 2780,
+          "pv": 3908,
+          "amt": 2000
+        },
+        {
+          "name": "Page E",
+          "uv": 1890,
+          "pv": 4800,
+          "amt": 2181
+        },
+        {
+          "name": "Page F",
+          "uv": 2390,
+          "pv": 3800,
+          "amt": 2500
+        },
+        {
+          "name": "Page G",
+          "uv": 3490,
+          "pv": 4300,
+          "amt": 2100
+        }
+      ]
+    }
+  }
+  render(){
+    return(
+      <BarChart width={730} height={250} data={this.state.data}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="pv" fill="#8884d8" />
+      <Bar dataKey="uv" fill="#82ca9d" />
+</BarChart>
+    );
+  }
+}
+export default BarChart;
   
--- client/src/components/ChartsLocations.js
+++ client/src/components/ChartsLocations.js
@@ -1,6 +1,14 @@
 import React from "react"
 import OverTime from "./LineChart"
-import MostWords from "./BarChart"
+import Bar from "./BarChart"
+import config from "../config"
+
+const red_over_time = config['red_over_time'];
+const twit_over_time = config['twit_over_time'];
+const red_pop_key_words = config['red_pop_key_words'];
+const twit_pop_key_words = config['twit_pop_key_words'];
+
+
 class ChartsLocation extends React.Component{
     render(){
       return(
@@ -11,12 +19,12 @@ class ChartsLocation extends React.Component{
               </div>
             </div>
             <div className="col-lg-12">
-            <OverTime name={"Twiter Data"}/>
-            <OverTime name={"Reddit Data"}/>
+            <OverTime name={"Twiter Data"} data={twit_over_time}/>
+            <OverTime name={"Reddit Data"} data={red_over_time}/>
             </div>
             <div className="col-lg-12">
-              <MostWords name={"Twitter Popular Keywords"}/>
-              <MostWords name={"Reddit Popular Keywords"}/>
+              <Bar name={"Twitter Popular Keywords"} data={twit_pop_key_words}/>
+              <Bar name={"Reddit Popular Keywords"} data={red_pop_key_words}/>
             </div>
          </div>
       );
