819662d8edaa702b748128c512e6df96e8f5def0
Bojken Sina <sinab@usi.ch>
2019-05-05 18:51:29 +0200

    Change structure of the data

--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -24,46 +24,32 @@ class OverTime extends React.Component{
       this.state = {
         data: [
           {
-            "name": "Page A",
-            "uv": 4000,
-            "pv": 2400,
-            "amt": 2400
+            "time": 2000,
+            "count":2100
           },
           {
-            "name": "Page B",
-            "uv": 3000,
-            "pv": 1398,
-            "amt": 2210
+            "time": 3000,
+            "count": 2200
           },
           {
-            "name": "Page C",
-            "uv": 2000,
-            "pv": 9800,
-            "amt": 2290
+            "time": 3200,
+            "count": 2300
           },
           {
-            "name": "Page D",
-            "uv": 2780,
-            "pv": 3908,
-            "amt": 2000
+            "time": 3300,
+            "count": 3400
           },
           {
-            "name": "Page E",
-            "uv": 1890,
-            "pv": 4800,
-            "amt": 2181
+            "name": 3400,
+            "count": 2000
           },
           {
-            "name": "Page F",
-            "uv": 2390,
-            "pv": 3800,
-            "amt": 2500
+            "time": 3600,
+            "count": 2200
           },
           {
-            "name": "Page G",
-            "uv": 3490,
-            "pv": 4300,
-            "amt": 2100
+            "time": 3800,
+            "count":2500
           }
         ]
       }
@@ -80,16 +66,16 @@ class OverTime extends React.Component{
         });
       });
     }
+    
     render(){
       return(
         <LineChart width={600} height={250} data={this.state.data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
+        <XAxis dataKey="time" />
+        <YAxis domain={[0, 10000]}/>
         <Tooltip/>
-        <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        <Legend/>
+        <Line type="monotone" dataKey="count" stroke="#8884d8" />
         </LineChart>
       );
     }
