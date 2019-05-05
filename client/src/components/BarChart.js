16bc31e2a71ef3b8bf41e771513d88c337d1ae3f
Bojken Sina <sinab@usi.ch>
2019-05-05 18:57:44 +0200

    Changed Structure aof data

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -25,46 +25,32 @@ class Graph extends React.Component{
     this.state = {
       data: [
         {
-          "name": "Page A",
-          "uv": 4000,
-          "pv": 2400,
-          "amt": 2400
+          "keyword":"android",
+          "count": 4000,
         },
         {
-          "name": "Page B",
-          "uv": 3000,
-          "pv": 1398,
-          "amt": 2210
+          "kwyqord": "apple",
+          "count": 3000 
         },
         {
-          "name": "Page C",
-          "uv": 2000,
-          "pv": 9800,
-          "amt": 2290
+          "keyword": "microsoft",
+          "count": 8000,
         },
         {
-          "name": "Page D",
-          "uv": 2780,
-          "pv": 3908,
-          "amt": 2000
+          "keyword": "Java",
+          "count": 3000
         },
         {
-          "name": "Page E",
-          "uv": 1890,
-          "pv": 4800,
-          "amt": 2181
+          "keyword": "C++",
+          "count": 3200,
         },
         {
-          "name": "Page F",
-          "uv": 2390,
-          "pv": 3800,
-          "amt": 2500
+          "keyword": "Pascal",
+          "count": 3000,
         },
         {
-          "name": "Page G",
-          "uv": 3490,
-          "pv": 4300,
-          "amt": 2100
+          "keyword": "Spring",
+          "count": 100,
         }
       ]
     }
@@ -73,12 +59,11 @@ class Graph extends React.Component{
     return(
       <BarChart width={600} height={250} data={this.state.data}>
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
+      <XAxis dataKey="keyword" />
       <YAxis />
       <Tooltip />
       <Legend />
-      <Bar dataKey="pv" fill="#8884d8" />
-      <Bar dataKey="uv" fill="#82ca9d" />
+      <Bar dataKey="count" fill="#8884d8" />
 </BarChart>
     );
   }
