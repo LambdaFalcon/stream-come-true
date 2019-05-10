eda9fc50e05f38a6757faa368a73ae36b0e275ad
falcon <aron.fiechter@gmail.com>
2019-05-10 12:18:21 +0200

    Add comments to API and main stream router

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,20 @@ const express = require('express');
 
 const stream = require('./../routes/stream');
 
+/**
+ * Create an API to be used by an express app.
+ * @example
+ *    const express = require('express');
+ *    const app = express();
+ *    app.use('/api/v1', api(config));
+ *
+ * @param {object} config server configuration
+ * @returns {object} Express.js router
+ */
 const api = (config) => {
   const router = express.Router();
 
+  // mount main router
   router.use('/', stream(config));
 
   return router;
