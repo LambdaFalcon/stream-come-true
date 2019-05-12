a94a85955f8695a98e2e293d6bc40d9ba8cbf903
falcon <aron.fiechter@gmail.com>
2019-05-13 01:01:36 +0200

    Add utility Date string functions to addFilters

--- server/middleware/addFilters.js
+++ server/middleware/addFilters.js
@@ -2,6 +2,23 @@ const debug = require('debug')('server:info');
 
 const asyncErrorCatch = require('../utils/asyncErrorCatch');
 
+const validDate = d => !!Date.parse(d);
+const fromBeforeTo = (from, to) => new Date(to) - new Date(from) > 0;
+const validDateFilters = (from, to) => validDate(from) && validDate(to) && fromBeforeTo(from, to);
+
+/**
+ * Subtract a given number of hours from a given date.
+ *
+ * @param {string} ds a date string
+ * @param {number} h a number of hours to subtract
+ * @returns {string} a new date string with the hours subtracted
+ */
+const minusHours = (ds, h) => {
+  const d = new Date(ds);
+  d.setHours(d.getHours() - h);
+  return d.toISOString();
+};
+
 /**
  * Middleware to attach filters present in req.query to req.filters.
  * Usually used in the main /:source router to add filters for all
