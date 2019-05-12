2ec2d441a6bfd5d88560b1ed83fb6cd45c46da38
Alexander Camenzind <alexander.camenzind@usi.ch>
2019-05-13 00:09:10 +0200

    it is now possible to make a selection on the line chart however it only prints the dates for now

--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -1,69 +1,115 @@
-import React from "react"
-import {Line,LineChart,CartesianGrid,XAxis,YAxis,Tooltip,Legend} from 'recharts'
+import React from "react";
+import {
+  Line,
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ReferenceArea
+} from "recharts";
 
-class OverTime extends React.Component{
-    render(){
-      return(
-        <div className="col-xs-6">
+class OverTime extends React.Component {
+  render() {
+    return (
+      <div className="col-xs-6">
         <div className="panel panel-default">
-          <div className="panel-heading">
-            {this.props.name}
-          </div>
+          <div className="panel-heading">{this.props.name}</div>
           <div className="panel-body">
             <Graph api= {this.props.api} textfilter={this.props.textfilter} x={this.props.x} y={this.props.y}/>
           </div>
         </div>
       </div>
-      );
+    );
+  }
+}
+class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refAreaLeft: "",
+      refAreaRight: "",
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.textfilter !== prevProps.textfilter) {
+      this.fetchData();
     }
   }
-  class Graph extends React.Component{
-    constructor(props){
-      super(props);
-      this.state = {
-        data: []
-      }
+  _resetSelectionState() {
+    this.setState(() => ({
+      refAreaLeft: "",
+      refAreaRight: ""
+    }));
+  }
+  filterTime() {
+    let { refAreaLeft, refAreaRight, data } = this.state;
+    //if the selected area has no width don't filter
+    if (refAreaLeft === refAreaRight || refAreaRight === "") {
+      this._resetSelectionState();
+      return;
     }
-  
-    componentDidMount(){
-      this.fetchData()
-    }
+    console.log("left date ", new Date(refAreaLeft))
+    console.log("right date ", new Date(refAreaRight))
+    //call calback
+    this._resetSelectionState();
+  }
 
-    componentDidUpdate(prevProps) {
-      if (this.props.textfilter !== prevProps.textfilter) {
-        this.fetchData();
-      }
-    }
-
-    fetchData(){
-      fetch(this.props.api + `?textfilter=${this.props.textfilter || ''}`)
+  fetchData() {
+    fetch(this.props.api + `?textfilter=${this.props.textfilter || ""}`)
       .then(res => {
         return res;
       })
-      .then(res=>res.json())
-      .then(res=>{
+      .then(res => res.json())
+      .then(res => {
         this.setState({
-            data: res
+          data: res
         });
       });
-    }
-    formatDate(time){
-        const date =  new Date(time);
-        return date.toLocaleTimeString("it-IT");
-    }
-    render(){
-      return(
-        <LineChart width={730} height={250} data={this.state.data}>
+  }
+  formatDate(time) {
+    const date = new Date(time);
+    return date.toLocaleTimeString("it-IT");
+  }
+  render() {
+    const { refAreaLeft, refAreaRight } = this.state;
+    return (
+      <LineChart
+        width={730}
+        height={250}
+        /*makeing the chart intercative*/
+        onMouseDown={e => this.setState({ refAreaLeft: e.activeLabel })}
+        onMouseMove={e =>
+          this.state.refAreaLeft &&
+          this.setState({ refAreaRight: e.activeLabel })
+        }
+        onMouseUp={this.filterTime.bind(this)}
+        data={this.state.data}
+      >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey={this.props.x} tickFormatter={this.formatDate} allowDuplicatedCategory={false}/>
         <YAxis minTickGap={5} tickSize={3} />
         <Tooltip filterNull={true} labelFormatter={this.formatDate} />
         <Legend/>
         <Line type="monotone" dataKey={this.props.y} stroke="#8884d8" activeDot={true}/>
+        {refAreaLeft && refAreaRight ? (
+          <ReferenceArea
+            x1={refAreaLeft}
+            x2={refAreaRight}
+            strokeOpacity={0.3}
+          />
+        ) : null}
         </LineChart>
       );
     }
   }
 
-  export default OverTime;
-  
+
+export default OverTime;
