2f87c0f0acd15c8731c73f802ebdd78c887efa83
falcon <aron.fiechter@gmail.com>
2019-05-12 16:15:25 +0200

    Add documentation to error handling middleware

--- server/utils/catch404.js
+++ server/utils/catch404.js
@@ -1,7 +1,16 @@
 const createError = require('http-errors');
 
+/**
+ * Middleware to catch all 404 errors. Use it by attaching it as the last middeware.
+ * If no previous route matches, this one will match and automatically forward a 404
+ * error to the error handling middleware.
+ *
+ * @example
+ *    const catch404 = require('./utils/catch404');
+ *    // attach all routers to app, then as last:
+ *    app.use(catch404);
+ */
 const catch404 = (req, res, next) => {
-  // pass control to next handler
   next(createError(404));
 };
 
