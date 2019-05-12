6b9a4e011988833a797aadb6840625a4f8f5638f
falcon <aron.fiechter@gmail.com>
2019-05-12 15:40:51 +0200

    Add documentation to source router

--- server/routes/source/index.js
+++ server/routes/source/index.js
@@ -5,6 +5,26 @@ const allData = require('./all');
 const itemsOverTime = require('./itemsOverTime');
 const popularKeywords = require('./popularKeywords');
 
+/**
+ * Create the router for a specific source.
+ *
+ * This router exposes three main routes:
+ * / (root): returns all data collected for the source
+ * /items_over_time: returns the items over time aggregation
+ * /popular_keywords: returns the popular keywords aggregation
+ *
+ * All routes also take query parameters:
+ * textfilter: filters data by text search (e.g. "google")
+ * timeframe: filters data by time frame (e.g. 5h)
+ *
+ * @example
+ *    const sourceRouter = require('./source');
+ *
+ *    router.use('/:source', sourceRouter(config));
+ *
+ * @param {object} config server configuration
+ * @returns {object} Express.js router
+ */
 const sourceRouter = (config) => {
   const router = express.Router();
 
