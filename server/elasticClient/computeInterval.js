98856b5d9f987a5922d70ff98930cbfe8b724216
falcon <aron.fiechter@gmail.com>
2019-05-13 03:38:54 +0200

    Change implementation of computeInterval to work on a range of seconds

--- server/elasticClient/computeInterval.js
+++ server/elasticClient/computeInterval.js
@@ -1,6 +1,6 @@
 const debug = require('debug')('server:elastic');
 
-const seconds = {
+const secondsPer = {
   d: 86400,
   h: 3600,
   m: 60,
@@ -13,10 +13,11 @@ const seconds = {
  * @param {('5m'|'5h'|'5d')} timeframe a time frame as a string
  * @returns {number} the timeframe in seconds
  */
-const decode = (timeframe) => {
+// eslint-disable-next-line no-underscore-dangle
+const _decode = (timeframe) => {
   const regex = /^(\d+)([dhms])$/;
   const [, num, kind] = regex.exec(timeframe);
-  return parseInt(num, 10) * seconds[kind];
+  return parseInt(num, 10) * secondsPer[kind];
 };
 
 /**
@@ -28,17 +29,18 @@ const decode = (timeframe) => {
 const encode = interval => `${interval}s`;
 
 /**
- * Compute a good interval of time to use as bucket given the current timeframe.
- * For now, the interval is about 1/100th of the timeframe.
+ * Compute a good interval of time to use as bucket given the current time frame.
+ * For now, the interval is about 1/100th of the time frame.
  *
- * @param {('5m'|'5h'|'5d')} timeframe the currently set timeframe
+ * @param {('5m'|'5h'|'5d')} seconds a time frame in seconds
  * @returns {string} a good interval
  */
-const computeInterval = (timeframe) => {
-  const decodedTimeframe = decode(timeframe);
-  const interval = Math.max(1, Math.round(decodedTimeframe / 100)) || 1;
+const computeInterval = (seconds) => {
+  const interval = Math.max(1, Math.round(seconds / 100)) || 1;
   const encodedInterval = encode(interval);
-  debug(`Computing interval: timeframe=${timeframe}, computed_interval=${encodedInterval}`);
+  debug(
+    `Computing interval: time frame in seconds = ${seconds}, computed interval = ${encodedInterval}`,
+  );
   return encodedInterval;
 };
 
