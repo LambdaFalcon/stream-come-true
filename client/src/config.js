0f573cb146a087c39a426e03670923c05c98a27e
falcon <aron.fiechter@gmail.com>
2019-05-14 14:03:58 +0200

    Change client config to use VM URL when built for production

--- client/src/config.js
+++ client/src/config.js
@@ -1,21 +1,26 @@
+const port = 9999;
+const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:9000/"
+    : `https://rhea.inf.usi.ch:${port}/`;
+
 const localhost = {
-    api: "http://localhost:9000",
-    api_v1: "http://localhost:9000/api/v1/",
-  }
-  
-  const config = {
-    red_over_time: localhost["api_v1"].concat("reddit/items_over_time"),
-    twit_over_time: localhost["api_v1"].concat("twitter/items_over_time"),
-    red_pop_key_words: localhost["api_v1"].concat("reddit/popular_keywords"),
-    twit_pop_key_words: localhost["api_v1"].concat("twitter/popular_keywords"),
-    red_user_over_time: localhost["api_v1"].concat("reddit/users_over_time"),
-    twit_user_over_time: localhost["api_v1"].concat("twitter/users_over_time"),
-    red_pop_users: localhost["api_v1"].concat("reddit/popular_users"),
-    twit_pop_users: localhost["api_v1"].concat("twitter/popular_users"),
-    red_sent_analysis: localhost["api_v1"].concat("reddit/sentiment"),
-    twit_sent_analysis: localhost["api_v1"].concat("twitter/sentiment"),
-    reddit_data: "http://localhost:9000/api/v1/reddit/",
-    twit_data: "http://localhost:9000/api/v1/twitter"
-  };
+  api_v1: `${baseUrl}api/v1/`
+};
+
+const config = {
+  red_over_time: localhost["api_v1"].concat("reddit/items_over_time"),
+  twit_over_time: localhost["api_v1"].concat("twitter/items_over_time"),
+  red_pop_key_words: localhost["api_v1"].concat("reddit/popular_keywords"),
+  twit_pop_key_words: localhost["api_v1"].concat("twitter/popular_keywords"),
+  red_user_over_time: localhost["api_v1"].concat("reddit/users_over_time"),
+  twit_user_over_time: localhost["api_v1"].concat("twitter/users_over_time"),
+  red_pop_users: localhost["api_v1"].concat("reddit/popular_users"),
+  twit_pop_users: localhost["api_v1"].concat("twitter/popular_users"),
+  red_sent_analysis: localhost["api_v1"].concat("reddit/sentiment"),
+  twit_sent_analysis: localhost["api_v1"].concat("twitter/sentiment"),
+  reddit_data: localhost["api_v1"].concat("reddit/"),
+  twit_data: localhost["api_v1"].concat("twitter/")
+};
 
 export default config;
