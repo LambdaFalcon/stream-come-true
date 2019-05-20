cdfb8a8fc78364938da39896271bcfae56fa7bc4
Bojken Sina <sinab@usi.ch>
2019-05-20 14:42:45 +0200

    Resolve "DateTimePicker"

---
+++ client/src/components/DateTime.js
@@ -0,0 +1,58 @@
+//react library
+import React from 'react'
+//importing the datetime-picker from react widgets library 
+import { DateTimePicker } from 'react-widgets'
+//importing moment and moment localizer to specify the format of the date
+import Moment from "moment"
+import momentLocalizer from 'react-widgets-moment'
+class DateTime extends React.PureComponent {
+  constructor(props){
+    super(props);
+    //It will save the dateto and the datefrom based on the component chnaged
+    this.state = {
+      datevalue: this.props.value
+    }
+  }
+  //It will update the state of the component whenever a change in the state is seen
+  componentDidUpdate(prevProps) {
+    if (this.props.value !== prevProps.value) {
+      this.setState(
+        {
+          datevalue: this.props.value
+        }
+      )
+    }
+  }
+  /**
+   * It renders the datetimepicker
+   * onSelect it is the event called whenever we select a new option in the datepicker
+   */
+  render(){
+    Moment.locale("en")
+    momentLocalizer()
+    const {datevalue} = this.state;
+    const {handleTimeIntervalChangeObjectBased} = this.props;
+    return(
+      <DateTimePicker
+      defaultValue={datevalue}
+      disabled={this.props.disabled}
+      value={this.state.datevalue}
+      onChange={(d)=>{
+        //react complains if we dont havae an onchange
+      }}
+      onSelect={(date)=>{
+        let o = {}
+        o[this.props.field] = date
+        handleTimeIntervalChangeObjectBased(o)
+      }
+    
+    }
+      />
+    );
+  }
+
+}
+/**
+ * We expor the datetime picker to be visible by other javascript classes
+ */
+export default  DateTime;
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,18 +1,49 @@
+
 import React from "react";
 class Search extends React.PureComponent {
+  /**
+   * Constructor for the react class
+   * It takes the props from the parent
+   * The state of the input will match to the state of the texfilter
+   * At first the state of the component will be an mpty string 
+   * since the textfilter will be an empty string 
+   */
   constructor(props) {
     super(props);
+    const {textfilter} = this.props
     this.state = {
-      input: ""
+      input: textfilter
     };
+  } 
+  /**
+   * 
+   * function called when the state of the current component
+   * has changed
+   * It check if the previous state is the same with the changed state
+   * If not you update the state 
+   */
+  componentDidUpdate(prevState){
+    const{input} = this.state;
+    const {textfilter} = this.props;
+    if(prevState.input !== input){
+      this.setState({
+        input: textfilter
+      })
+    }
   }
+  /**
+   * function that render the React component 
+   * based on html syntax
+   */
   render() {
+    const {input} = this.state
     return (
-      <div class="form-group">
+      <div className="form-group">
         <input
           type="text"
           className="form-control input-md"
           placeholder="Keyword"
+          value={input}
           onChange={this.props.handler}
         />
       </div>
@@ -20,4 +51,5 @@ class Search extends React.PureComponent {
     );
   }
 }
+//In order to import class in other javascript files.
 export default Search;
