03b5c0286e2e35b6b56044059885d1141e4c4af8
mielsi <ruizb@usi.ch>
2019-05-23 19:00:28 +0200

    Bold tags in test

--- server/test/apiv1/source/index.test.js
+++ server/test/apiv1/source/index.test.js
@@ -130,7 +130,7 @@ describe('/api/v1/:source ROUTE', function sourceTest() {
       const res = await getRequest(`${redditRoute}?textfilter=${textfilter}`);
       res.body
         .map(item => item.text.toLowerCase())
-        .should.all.have.string(`<em>${textfilter}</em>`);
+        .should.all.have.string(`<b>${textfilter}</b>`);
     });
   });
 
