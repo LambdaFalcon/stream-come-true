c1c6285dac4383b15aea164e8d03b06fa429e49a
Alexander Camenzind <alexander.camenzind@usi.ch>
2019-05-26 00:18:26 +0200

    legend is now only shown if more than one value is being displayed in a graph

--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactEcharts from "echarts-for-react";
 
-const DAY = 24*60*60*1000
+const DAY = 24 * 60 * 60 * 1000;
 
 class OverTime extends React.Component {
   render() {
@@ -9,8 +9,8 @@ class OverTime extends React.Component {
       onChangeTimeInterval,
       textfilter,
       timefilter,
-      x,
-      y,
+      bars,
+      lines,
       api
     } = this.props;
     return (
@@ -23,12 +23,11 @@ class OverTime extends React.Component {
               textfilter={textfilter}
               timefilter={timefilter}
               onChangeTimeInterval={onChangeTimeInterval}
-              x={x}
-              y={y}
-              sentiment={this.props.sentiment}
               refreshing={this.props.refreshing}
               labelX={this.props.labelX}
               labelY={this.props.labelY}
+              bars={bars}
+              lines={lines}
             />
           </div>
         </div>
@@ -37,8 +36,15 @@ class OverTime extends React.Component {
   }
 }
 class Graph extends React.Component {
+  static defaultProps = {
+    bars: [],
+    lines: []
+  };
   constructor(props) {
     super(props);
+    if (props.bars.length + props.lines.length < 1) {
+      throw "At least one bar or line needs to be defined";
+    }
     this.state = {
       option: this.getDefaultOption(),
       data: []
@@ -104,65 +110,91 @@ class Graph extends React.Component {
     const date = new Date(time);
     return date.toLocaleTimeString("it-IT");
   }
-  formatDateTime(time){
+  formatDateTime(time) {
     const date = new Date(time);
     return date.toLocaleString("it-IT");
   }
   /**
-   * 
-   * @param {[series information]} params 
+   *
+   * @param {[series information]} params
    */
-  formatTooltip(params){
-    let {option} = this.state
-    let timeAxis = option.xAxis[0].data
-    let index = params[0].dataIndex
+  formatTooltip(params) {
+    let { option } = this.state;
+    let timeAxis = option.xAxis[0].data;
+    let index = params[0].dataIndex;
 
-    let fromString = this.formatDateTime(timeAxis[index].actual)
-    let toString = timeAxis[index+1] ? this.formatDateTime(timeAxis[index+1].actual) : "now"
-    let ret = `<div>${fromString} - ${toString} </di>`
-    params.forEach(item =>{
-      ret+= `<div>${item.seriesName}: ${item.value}</div>`
-    })
-    return ret
+    let fromString = this.formatDateTime(timeAxis[index].actual);
+    let toString = timeAxis[index + 1]
+      ? this.formatDateTime(timeAxis[index + 1].actual)
+      : "now";
+    let ret = `<div>${fromString} - ${toString} </di>`;
+    params.forEach(item => {
+      ret += `<div>${item.seriesName}: ${item.value}</div>`;
+    });
+    return ret;
   }
   updateGraph(data) {
     const option = this.getDefaultOption();
     //note data.length is not always 100 it is usually in [99,101]
 
-    //if time between first and last item is more than 24h we display dates
-    let first = data[0].time
-    let last = data[data.length-1].time
-      option.xAxis[0].data = data.map(item => {
-        return {
-          value: last - first < DAY ? this.formatTime(item.time) : this.formatDate(item.time),
-          actual:item.time}
-      });
-    
-
-    
-    
-    option.series[0].data = data.map(item => item.count);
-    option.series[1].data = data.map(item => item.positive_count);
-    option.series[2].data = data.map(item => item.negative_count);
-
+    //if time between first and last item is more than 24h we display dates instead of times
+    let first = data[0].time;
+    let last = data[data.length - 1].time;
+    option.xAxis[0].data = data.map(item => {
+      return {
+        value:
+          last - first < DAY
+            ? this.formatTime(item.time)
+            : this.formatDate(item.time),
+        actual: item.time
+      };
+    });
+    //updating data for all series
+    option.series.forEach(item => {
+      item.data = data.map(x=> x[item.dataKey])
+    })
     this.setState({
       option: option
     });
   }
+  /**
+   * returns the echarts options that are not dependent on the fetched data
+   */
   getDefaultOption() {
+    let { bars, lines } = this.props;
+    let series = [];
+    lines.forEach(conf =>
+      series.push({
+        type: "line",
+        name: conf.name,
+        color: conf.color,
+        dataKey: conf.dataKey,
+        smooth: true,
+        data: []
+      })
+    );
+    bars.forEach(conf =>
+      series.push({
+        type: "bar",
+        stack: conf.stack,
+        name: conf.name,
+        color: conf.color,
+        dataKey: conf.dataKey,
+        data: []
+      })
+    );
     return {
       title: {
         // text:'We could put a title here as well',
       },
       tooltip: {
         trigger: "axis",
-        formatter:(params)=>{return this.formatTooltip(params)}
+        formatter: params => {
+          return this.formatTooltip(params);
+        }
       },
-      legend: {
-        orient:"vertical",
-        right:0,
-        bottom:"50%"
-      },
+      //only displaying legend if we display more than one thing
+      legend: series.length > 1 ? {orient: "horizontal"} : false,
       dataZoom: {
         show: false,
         start: 0,
@@ -181,40 +213,14 @@ class Graph extends React.Component {
           scale: true,
           name: "count",
           nameLocation: "middle",
-          nameTextStyle:{
+          nameTextStyle: {
             padding: [0, 0, 30, 0]
           },
           min: 0,
           boundaryGap: [0.2, 0.2]
         }
       ],
-      series: [
-        {
-          name: "count",
-          color: "rgb(117,108,206)",
-          type: "line",
-          smooth: true,
-          data: []
-        },
-        {
-          name: "positive",
-          type: "bar",
-          stack:"one",
-          color: "#2A9D8F",
-          lineStyle:{type: 'dashed'},
-          smooth: true,
-          data: []
-        },
-        {
-          name: "negative",
-          color: "#E9C46A",
-          type: "bar",
-          stack:"one",
-          lineStyle:{type: 'dashed'},
-          smooth: true,
-          data: []
-        }
-      ]
+      series: series
     };
   }
 
