bcb4355e0dc9af2da3fa04ce7f23b69f38927359
mielsi <ruizb@usi.ch>
2019-05-25 14:03:19 +0200

    Added spidering on hashtag graph

--- client/src/components/HashtagGraph.js
+++ client/src/components/HashtagGraph.js
@@ -12,14 +12,13 @@ class Hashtags extends React.PureComponent{
                             {this.props.name}
                         </div>
                         <div className="panel-body">
-                        <HashtagGraph id='networkg' timefilter={this.props.timefilter} api={this.props.api} textfilter={this.props.textfilter}/>
+                        <HashtagGraph timefilter={this.props.timefilter} api={this.props.api} textfilter={this.props.textfilter} spidering={this.props.spidering} onNodeSpidering={this.props.onNodeSpidering}/>
                         </div>
                     </div>
                 </div>
             </div>
         );
     }
-
 }
 
 class HashtagGraph extends React.Component {
@@ -30,6 +29,7 @@ class HashtagGraph extends React.Component {
           data: {}
         };
         this.textfilter = "";
+        this.spidering = "";
       }
     
       componentDidMount() {
@@ -42,6 +42,9 @@ class HashtagGraph extends React.Component {
         }
         if (this.props.timefilter !== prevProps.timefilter) {
           this.fetchData();
+        }
+        if (this.props.spidering !== prevProps.spidering) {
+          this.fetchHashtagSpidering();
         }
       }
     
@@ -62,11 +65,57 @@ class HashtagGraph extends React.Component {
             });
           });
       }
+
+      fetchHashtagSpidering() {
+        fetch(
+          this.props.api + 
+            `?textfilter=${this.props.textfilter || ""}` +
+            (this.props.timefilter ? this.props.timefilter : "") +
+            `&spidering=${this.props.spidering}`, {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(this.props.spidering)     
+          })
+          .then(res => {
+            return res;
+          })
+          .then(res => res.json())
+          .then(res => {
+            this.setState({
+              data: res
+            })
+          });
+    }
     
       changeTextFilter(keyword) {
         const { onTextFilterChange } = this.props;
         onTextFilterChange(keyword);
       }
+
+      onNodeSpidering(node) {
+        const { onNodeSpidering } = this.props;
+        onNodeSpidering(node);
+      }
+
+      handleClick = hashtagNode => {  
+        // click on a node to expand the network
+        var hashtag = hashtagNode.data.term;
+        var exclude = this.state.data.vertices.filter(node => {
+          return node.term !== hashtagNode.data.term
+        }).map(node => {
+          return node.term;
+        });
+
+        var obj = {
+          hashtag: hashtag,
+          exclude: exclude
+        }
+        this.setState({
+          spidering: obj
+        });
+        
+        this.onNodeSpidering(obj);
+      };
 
     render() {
       const getOption = () =>({
@@ -75,43 +124,70 @@ class HashtagGraph extends React.Component {
         color: '#157DEC'
       });
       const chart = this.graphRef.current;
-      
       // if chart has been initialized and data exists
       if( chart !== null && this.state.data.vertices ){
         const mychart = chart.getEchartsInstance();
-        // add the rest of the properties
+        if(this.state.data.vertices.length > 0){
+          console.log(this.state.data.vertices.length)
+          // add the rest of the properties
+          mychart.setOption({
+            title: {
+              show: false
+            },
+            series : [{
+              type: 'graph',
+              layout: 'force',
+              animation: false,
+              label: {
+                  normal: {
+                      position: 'right',
+                      formatter: '{b}',
+                      show: true
+                  }
+              },
+              draggable: true,
+              focusNodeAdjacency: true,
+              force: {
+                  initLayout: 'circular',
+                  edgeLength: 100,
+                  repulsion: 300,
+                  gravity: 0.2
+              },
+              data: this.state.data.vertices.map(function (node, index) {
+                  node.id = index;
+                  node.name = node.term.replace('#','');
+                  node.symbolSize = 10;
+                  return node;
+              }),
+            edges: this.state.data.connections
+            }] 
+          });
+      }
+       else {  
+        mychart.clear()
         mychart.setOption({
-          series : [{
-            type: 'graph',
-            layout: 'force',
-            animation: false,
-            label: {
-                normal: {
-                    position: 'right',
-                    formatter: '{b}',
-                    show: true
-                }
+          title: {
+            textStyle:{
+              fontWeight: 'normal',
+              color: 'grey',
+              fontSize: 18
             },
-            draggable: true,
-            focusNodeAdjacency: true,
-            force: {
-                initLayout: 'circular',
-                edgeLength: 100,
-                repulsion: 300,
-                gravity: 0.2
-            },
-            data: this.state.data.vertices.map(function (node, index) {
-              node.id = index;
-              node.name = node.term.replace('#','');
-              node.symbolSize = 18;
-              return node;
-            }),
-          edges: this.state.data.connections
-        }] 
-      });
+            top: '50%',
+            left: '50%',
+            right: '50%',
+            show: true,
+            text: 'No data available'
+          }
+        });
     }
-    return (  
-      <ReactEcharts ref={this.graphRef} option={getOption()} style={{ height: 300 }}/>
+  }
+
+    const onEvents = {
+      'click': this.handleClick
+    }
+
+    return ( 
+      <ReactEcharts ref={this.graphRef} option={getOption()} style={{ height: 300 }} onEvents={onEvents}/>
         );
     }
 }
