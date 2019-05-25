e311cbd42e45ec0083af0f1b6dd1d8ee591500a4
Bojken Sina <sinab@usi.ch>
2019-05-25 17:26:35 +0200

    Succesful test reddit user over time

--- client/cypress/integration/App.js
+++ client/cypress/integration/App.js
@@ -53,4 +53,16 @@ describe("Check API Requests", function() {
       delay: 1000
     });
   });
+  it("Reddit Users Over Time", function() {
+    cy.server({
+      method: "GET",
+      url: "http://localhost/9000",
+      delay: 1000
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:9000/api/v1/reddit/users_over_time",
+      delay: 1000
+    });
+  });
 });
