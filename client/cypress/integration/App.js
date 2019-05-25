a1b9c104c4d29ca1b0b857b1b7bb411f7646f6b1
Bojken Sina <sinab@usi.ch>
2019-05-25 17:30:36 +0200

    Succesful test tweeter popular users

--- client/cypress/integration/App.js
+++ client/cypress/integration/App.js
@@ -89,4 +89,16 @@ describe("Check API Requests", function() {
       delay: 1000
     });
   });
+  it("Tweeter Popular User", function() {
+    cy.server({
+      method: "GET",
+      url: "http://localhost/9000",
+      delay: 1000
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:9000/api/v1/reddit/popular_users",
+      delay: 1000
+    });
+  });
 });
