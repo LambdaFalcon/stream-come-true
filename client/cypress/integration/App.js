5797cd8b53a358deaf54d91941eed27782278989
Bojken Sina <sinab@usi.ch>
2019-05-25 16:53:18 +0200

    Succesful test of react app and Reddit over time api request

---
+++ client/cypress/integration/App.js
@@ -0,0 +1,20 @@
+describe("Check Website", function() {
+  it("Website", function() {
+    cy.visit("/");
+  });
+});
+
+describe("Check API Requests", function() {
+  it("Reddit Over Time", function() {
+    cy.server({
+      method: "GET",
+      url: "http://localhost/9000",
+      delay: 1000
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost/9000/api/v1/reddit/items_over_time/",
+      status: 200
+    });
+  });
+});
