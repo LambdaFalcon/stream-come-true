455153ee6aa44441473ad5cf744adc0e16ebd190
Bojken Sina <sinab@usi.ch>
2019-05-25 16:56:40 +0200

    Succesful test twitter over time

--- client/cypress/integration/App.js
+++ client/cypress/integration/App.js
@@ -14,7 +14,19 @@ describe("Check API Requests", function() {
     cy.route({
       method: "GET",
       url: "http://localhost/9000/api/v1/reddit/items_over_time/",
-      status: 200
+      delay: 1000
+    });
+  });
+  it("Twitter Over Time", function() {
+    cy.server({
+      method: "GET",
+      url: "http://localhost/9000",
+      delay: 1000
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:9000/api/v1/twitter/items_over_time",
+      delay: 1000
     });
   });
 });
